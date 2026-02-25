5712641bc2f69b8bc570a635d74c8f86133a28bc
subham12r <rikk4335@gmail.com>
2026-02-25 13:21:14 +0530

    added cors

--- src/proxy.ts
+++ src/proxy.ts
@@ -49,8 +49,9 @@ export async function proxy(request: NextRequest) {
   response.headers.set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   response.headers.set("Access-Control-Allow-Headers", "Content-Type, Authorization");
   response.headers.set("Access-Control-Max-Age", "86400");
-
+  
   if (request.method === "OPTIONS") {
+    console.log("Handled preflight OPTIONS request");
     return response;
   }
 
